f25c42619b5ea13050dc757866d0a512ab171b58
Andrei <142443853+hjzres@users.noreply.github.com>
2024-03-25 11:42:53 -0400

    each note has a different number

--- ToDoList.js
+++ ToDoList.js
@@ -1,10 +1,12 @@
 const addNoteButton = document.getElementById("addNote");
+let noteNum = 0;
 
 
 addNoteButton.addEventListener('click', () => {
     const noteDiv = document.createElement("div");
     noteDiv.className = 'note';
-    noteDiv.id = 'note';
+    noteDiv.id = 'note' + noteNum;
     document.body.appendChild(noteDiv);
+    noteNum++;
 });
 
