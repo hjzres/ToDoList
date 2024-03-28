4442f8ff835b9676143caf3ccf4ed0281793e4ba
Andrei <142443853+hjzres@users.noreply.github.com>
2024-03-28 10:16:05 -0400

    made the modal work

--- ToDoList.js
+++ ToDoList.js
@@ -1,12 +1,26 @@
-const addNoteButton = document.getElementById("addNote");
+const addNoteButton = document.getElementById("add-to-do");
+const modal = document.getElementById("modal");
+
 let noteNum = 0;
 
 
 addNoteButton.addEventListener('click', () => {
+    modal.style.display = "block";
+});
+
+let NewNote = () => {
     const noteDiv = document.createElement("div");
-    noteDiv.className = 'note';
-    noteDiv.id = 'note' + noteNum;
+    noteDiv.className = 'to-do';
+    noteDiv.id = 'to-do' + noteNum;
+    
+    const checkbox = document.createElement("input");
+    checkbox.type = "checkbox";
+    noteDiv.appendChild(checkbox);
+
+    const text = document.createElement("p");
+    text.textContent = "New Note";
+    noteDiv.appendChild(text);
     document.body.appendChild(noteDiv);
     noteNum++;
-});
+}
 
